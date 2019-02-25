0fc2ef509828dabf2988e362e13a424c509657bc
danielpfeffer <daniel.pfeffer@breathless-pictures.at>
2019-02-25 13:00:25 +0100

    Initial commit

---
+++ src/main/java/main/SocketServer.java
@@ -0,0 +1,37 @@
+package main;
+
+/**
+ *
+ * @author H. Lackinger
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+import org.glassfish.tyrus.server.Server;
+ 
+public class SocketServer {
+ 
+    public static void main(String[] args) {
+        runServer();
+    }
+ 
+    private static void runServer() {
+        org.glassfish.tyrus.server.Server server = new org.glassfish.tyrus.server.Server("localhost",
+                                   8025, 
+                                   "/websockets", 
+                                   ChatEndpoint.class);
+ 
+        try {
+            server.start();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+            System.out.print("Please press a key to stop the chat-server.");
+            reader.readLine();
+        } catch (Exception me) {
+            //if u can
+            throw new RuntimeException(me);
+        } finally {
+            server.stop();
+        }
+    }
+}
